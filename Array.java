d4e692c575044760adfe2e0f2620dca62904e682
TOPI <topivali04@gmail.com>
2025-02-21 15:47:09 +0530

    wit a new branch

---
+++ Array.java
@@ -0,0 +1,22 @@
+package DATATYPES;
+
+public class Array {
+
+    public static void main(String[] args){
+
+        int[] a={1,2,3,4,5,6,7,8,9};
+        System.out.println("First element: " + a[0]);
+        System.out.println("Last element: " + a[a.length - 1]);
+
+
+        int[][]B={
+
+            {2,3,4,5,6},{7,8,9,10,11},{12,13,14,15,16}
+        };
+
+
+
+
+    }
+
+}
---
+++ selection.java
@@ -0,0 +1,15 @@
+public class selection {
+    public static void main(String[] args){
+
+
+        int A = 15;
+        if (A == 0) {
+            System.out.println("The number is positive.");
+        }else if (A < 13) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+    
+}
